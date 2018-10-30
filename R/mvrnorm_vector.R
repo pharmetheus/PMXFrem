aa3342ca16f94ac45ac33b969c41536db6ad887e
Niclas Jonsson <Niclas.Jonsson@pharmetheus.com>
2018-10-30 11:00:51 +0100

    Added Roxygen to mvnorm

--- R/mvrnorm_vector.R
+++ R/mvrnorm_vector.R
@@ -1,20 +1,35 @@
-####################################################################################
-##
-## Generate a vector of multivariate samples,
-## mean vector mu
-## covariance matrix sigma
-## a vector where 1 indicates that a mu is fixed (note that size of sigma is the size
-## of the number of unfixed parameters
-#  iSampleIndex = 0 (return mu), 1=return first sample vector,
-## n = return n-sample vector
-## dSeed, the seed to use while sampling, NULL=no seed is set
-##
-## by Joakim Nyberg 2014
-####################################################################################
-                                        
+#' mvrnorm_vector
+#'
+#' @description Samples from a NONMEM .cov matrix
+#' @param mu Mean vector
+#' @param sigma Covariace matrix
+#' @param fixed_mu To be described
+#' @param dSeed The seed number
+#' @param iSampleIndex To be described
+#' @return A vector.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' ## To extract the final parameter estimates
+#' dfext    <- subset(getExt(extFile = "myExtFile.ext"),ITERATION=="-1000000000")
+#' }                                     
 mvrnorm_vector <- function(mu,sigma,fixed_mu=NULL,dSeed=NULL,iSampleIndex=1)
  {
-   require(MASS) #For mvrnorm
+  ####################################################################################
+  ##
+  ## Generate a vector of multivariate samples,
+  ## mean vector mu
+  ## covariance matrix sigma
+  ## a vector where 1 indicates that a mu is fixed (note that size of sigma is the size
+  ## of the number of unfixed parameters
+  #  iSampleIndex = 0 (return mu), 1=return first sample vector,
+  ## n = return n-sample vector
+  ## dSeed, the seed to use while sampling, NULL=no seed is set
+  ##
+  ## by Joakim Nyberg 2014
+  ####################################################################################
+     require(MASS) #For mvrnorm
    if (iSampleIndex==0) return (mu)
    if (is.null(fixed_mu)) fixed_mu<-rep(0,length(mu))
    tmp_mu<-mu[which(fixed_mu==0)] #Get the non-fixed mu
