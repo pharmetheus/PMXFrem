1166b9f19d9f6d879c2e3a6d2e56472591d09892
Joakim Nyberg <joakim.nyberg@pharmetheus.com>
2018-11-01 09:53:47 +0100

    Forest FREM functions

--- R/getForestDF.R
+++ R/getForestDF.R
@@ -2,12 +2,18 @@
 #'
 #' @description Get a data frame with Forest border for each univariate or multivariate covariate (and value(s)) in the input data frame
 #' @param dfCovs A data frame with covariates to include, if a covariate value is set -99 or NA, they are assumed missing and hence not include in the FFEM transformations
+#' @param cdfCovsNames A string vector 
 #' @param functionList A list of functions with input (thetas, coveffects and ...) for in which the change from the reference value will be calculated
 #' @param functionListName A vector of strings (names) of the parameters for each function in the functionList
 #' @param nobaseThetas the number of structural thetas in the model
 #' @param dfParameters A data frame with parameter samples from the uncertainty distribution. The final estimate vector is assumed to be at the first row. The column order is assumed the same as in the NONMEM ext file except the ITERATION and OBJ columns which are not expected.
+#' @param quiet If output should be allowed during the function call, default= FALSE,
 #' @param probs A vector of probabilities (uncertainty of the parameters)
 #' @param dfRefRow A data frame (one row) with the covariate values that will be used as the reference, if NULL the typical subject is used as reference
+#' @param cGrouping A vector of numbers on how to group the y-axis of the forest plot, the length of the vector should match the number of rows in dfCovs. If NULL (default) a educated guess of the grouping will be set
+#' @param fixedSpacing A boolean (TRUE/FALSE) if fixed spacing between covariate groups should be applied, otherwise the y coordinates are calculated relative to the number of groups and numbers of covriates within a group. If fixed spacing are used, groupdist and withingroupdist will be used as well.
+#' @param groupeddist A number defining the y distance between groups of covariates
+#' @param withingroupeddist A number defining the y distance within groups of covariates
 
 #' 
 #'
@@ -19,12 +25,33 @@
 #' \dontrun{
 #' dfForest <- getForestDF(dfCovs)
 #' }
-getForestDF <- function(dfCovs,functionList=list(function(thetas,coveffects,...){return(thetas[1]*exp(coveffects[1]))}),functionListName="PAR1",noBaseThetas, noCovThetas, noSigmas, noParCov = noBaseThetas, dfParameters,
+getForestDF <- function(dfCovs,cdfCovsNames=NULL,functionList=list(function(thetas,coveffects,...){return(thetas[1]*exp(coveffects[1]))}),functionListName="PAR1",noBaseThetas, noCovThetas, noSigmas, noParCov = noBaseThetas, dfParameters,
                         parNames = paste("Par", 1:noParCov, sep = ""), covNames = paste("Cov",1:noCovThetas, sep = ""),
-                        availCov = covNames, quiet = FALSE,probs=c(0.025,0.5,0.0975),dfRefRow=NULL) {
+                        availCov = covNames, quiet = FALSE,probs=c(0.025,0.5,0.975),dfRefRow=NULL,cGrouping=NULL,fixedSpacing=TRUE,groupdist=0.2,withingroupdist=0.1) {
 
+  
   resList<-list()
   dfCovs[is.na(dfCovs)]<--99
+  
+  getGroups<-function(df){
+    cGroups<-c()
+    cUnique<-c()
+    iGroup<-0
+    for (i in 1:nrow(df)) {
+      tmp<-paste0(names(dfCovs[i,])[as.numeric(dfCovs[i,])!=-99],collapse=",")
+      if (tmp %in% cUnique) {
+        tmpl=which(tmp==cUnique)
+        cGroups<-c(cGroups,tmpl)
+      } else {
+        iGroup<-iGroup+1
+        cGroups<-c(cGroups,iGroup)
+        cUnique<-c(cUnique,tmp)
+      }
+    }
+    return(cGroups)
+  }
+  if (is.null(cGrouping)) cGrouping<-getGroups(dfCovs)
+  
   counter<-1
   for (k in 1:nrow(dfParameters)) {
     dfext<-cbind(first = 0, dfParameters[k,]) #Dummy to get same format as ext file
@@ -47,7 +74,7 @@ getForestDF <- function(dfCovs,functionList=list(function(thetas,coveffects,...)
       data47_jxrtp <- dfCovs[i,]
       for(j in 1:length(parNames)) {
         ffem_expr<-str_replace_all(ffemObj$Expr[j],pattern = "data\\$",replacement = "data47_jxrtp$")
-        coveffects[j] <- as.numeric(eval(parse(text=ffem_expr)))
+        if (length(names(dfCovs[i,])[as.numeric(dfCovs[i,])!=-99])!=0) coveffects[j] <- as.numeric(eval(parse(text=ffem_expr)))
         if (!is.null(dfRefRow)) {
            ffem_expr_base<-str_replace_all(ffemObjRef$Expr[j],pattern = "data\\$",replacement = "dfRefRow$")
            coveffects_base[j]<-as.numeric(eval(parse(text=ffem_expr_base)))
@@ -67,15 +94,37 @@ getForestDF <- function(dfCovs,functionList=list(function(thetas,coveffects,...)
   }
   dfres<-as.data.frame(data.table::rbindlist(resList))
 
+  getCovNameString <- function(dfrow) {
+    strName<-""
+    colnames<-names(dfrow)
+    for (i in 1:ncol(dfrow)) {
+      if (dfrow[1,i]!=-99) {
+        if (strName=="") {
+          strName<-paste0(colnames[i],"=",dfrow[1,i])
+        } else {
+          strName<-paste0(strName,", ",colnames[i],"=",dfrow[1,i])
+        }
+      }
+    }
+    if (strName=="") strName<-"Ref cov"
+    return (strName)
+  }
+  
   dfret<-data.frame()
   for (i in 1:nrow(dfCovs)) {
+    if (is.null(cdfCovsNames)) {
+      covname<-getCovNameString(dfCovs[i,])
+    } else {
+      covname<-cdfCovsNames[i]
+    }
+    group<-cGrouping[i]
     for (j in 1:length(functionList)) {
       dft<-dfres[dfres$COVS==i & dfres$NAME==functionListName[j],] #Subset each covariate and parameter
       quant<-quantile(dft$VALUE,probs = probs,names = FALSE)
       mean_base<-mean(dft$VALUEBASE)
       median_base<-median(dft$VALUEBASE)
       true_base<-dft$VALUEBASE[dft$ITER==1] #Assume first parameter vector in dfParameters are the base estimated parameter vector
-      dfrow<-cbind(dfCovs[i,],data.frame(COVNUM=i,PARAMETER=functionListName[j],REFMEAN=mean_base,REFTRUE=true_base,REFMEDIAN=median_base))
+      dfrow<-cbind(dfCovs[i,],data.frame(GROUP=group,COVNUM=i,COVNAME=covname,PARAMETER=functionListName[j],REFMEAN=mean_base,REFTRUE=true_base,REFMEDIAN=median_base))
       for (k in 1:length(probs)) {
         dfp<-data.frame("X1"=1)
         dfp[[paste0("q",k)]]<-quant[k]
@@ -83,6 +132,32 @@ getForestDF <- function(dfCovs,functionList=list(function(thetas,coveffects,...)
         names(dfrow)[ncol(dfrow)]<-paste0("q",k)
       }
       dfret<-rbind(dfret,dfrow)
+    }
+  }
+  
+  
+  if (!fixedSpacing) {
+  #Relative sizes
+  dfret$Y<-NA
+  for (i in 1:length(sort(unique(dfret$GROUP)))) {
+    dft<-dfret[dfret$GROUP==sort(unique(dfret$GROUP))[i],]
+    num_in_group<-nrow(dft)/length(unique(dfret$PARAMETER))
+    for (n in 1:length(unique(dft$COVNUM))) {
+      dfret$Y[dfret$GROUP==sort(unique(dfret$GROUP))[i] & dfret$COVNUM==unique(dft$COVNUM)[n]]<-(i-1)+n/(num_in_group+1)
+    }
+  }
+  } else {
+  #Fixed sizes
+  dfret$Y<-NA
+  a<-0
+  for (i in 1:length(sort(unique(dfret$GROUP)))) {
+    dft<-dfret[dfret$GROUP==sort(unique(dfret$GROUP))[i],]
+    num_in_group<-nrow(dft)/length(unique(dfret$PARAMETER))
+    for (n in 1:length(unique(dft$COVNUM))) {
+      dfret$Y[dfret$GROUP==sort(unique(dfret$GROUP))[i] & dfret$COVNUM==unique(dft$COVNUM)[n]]<-a
+      a<-a+withingroupdist
+    }
+    a<-a+groupdist
     }
   }
   return(dfret)
---
+++ R/plotForestDF.R
@@ -0,0 +1,75 @@
+#' plotForestDF()
+#'
+#' @param df the forest plot data frame, should be based on a call to getForestDF or manually created accfording to the getForestDF structure
+#' @param plotRelative if the plot should be based on the relative scale to the reference (or parameter scale), default = TRUE
+#' @param addTable if a CI table should be added to the plot, default = TRUE
+#' @param strxlab x-label, default = "Covariate-Parameter effect"
+#' @param strylab y-label, default = ""
+#' @param decdig number of digits after the comma in the CI table, only used with addTable = TRUE, default=2 
+#' @param percentscalexmax increase of xmax, default 1.5 (fraction of x-axis), only used with addTable = TRUE
+#' @param xtextoffset textoffset, default 0.25 on x-axis scale, only used with addTable = TRUE
+#' @param vlinecol color of vertical reference line, default "lightgrey"
+#' @param vlinesize size of vertical reference line, default 1.5
+#' @param pointsize size of geom point (e.g. median value), default 3
+#' @param errorbarsize size of errorbars, default 1
+#' @param freescalex_wrap if wrap as free_x or not, default based on plotRelative and addTable
+#'
+#' @return a ggplot2 object with a forest plot
+#' @export
+#'
+#' @examples
+plotForestDF <-function(df,plotRelative=TRUE,addTable=TRUE,strxlab="Covariate-Parameter effect",strylab="",decdig=2,
+                        percentscalexmax=1.5,xtextoffset=0.25,vlinecol="lightgrey",vlinesize=1.5,pointsize=3,errorbarsize=1,
+                        freescalex_wrap=(!plotRelative || addTable))
+{
+  
+  if (addTable) { #Insert CI table
+    df$XMAX<-NA
+    df<-plyr::ddply(df,  .variables=c("PARAMETER"), .fun = function(x,pr=plotRelative){
+      if (pr) { 
+        x$XMAX<-max(x$q3/x$REFMEDIAN)
+      } else {
+        x$XMAX<-max(x$q3)
+      }
+      return(x)
+    })
+  }
+  
+  p<-ggplot(df)
+  
+  if (plotRelative) {
+    #Based on relative  values
+    p<-p+geom_vline(aes(xintercept=1),size=vlinesize,color=vlinecol)
+    p<-p+geom_errorbarh(aes(y=Y,xmin=q1/REFMEDIAN,xmax=q3/REFMEDIAN,x=q2/REFMEDIAN,color=as.factor(GROUP)),size=errorbarsize)
+    p<-p+geom_point(aes(y=Y,x=q2/REFMEDIAN,color=as.factor(GROUP)),size=pointsize)
+  } else {
+    #Based on actual values
+    p<-p+geom_vline(aes(xintercept=REFMEDIAN),size=vlinesize,color=vlinecol)
+    p<-p+geom_errorbarh(aes(y=Y,xmin=q1,xmax=q3,x=q2,color=as.factor(GROUP)),size=errorbarsize)
+    p<-p+geom_point(aes(y=Y,x=q2,color=as.factor(GROUP)),size=pointsize)
+  }
+  
+  p<-p + scale_y_continuous(breaks=unique(df$Y),labels = unique(df$COVNAME))
+  p<-p+guides(color=FALSE)
+  p<-p+theme(panel.grid.minor.y = element_blank(),panel.grid.major.y = element_blank(), axis.ticks.y = element_blank())
+  p<-p+ylab(strylab)+xlab(strxlab)
+
+  if (addTable) { #Insert CI table
+
+    if (plotRelative) {
+      p<-p+geom_text(aes(y=Y,x=XMAX+xtextoffset,label=paste0(formatC(q2/REFMEDIAN,format="f",decdig)," [",formatC(q1/REFMEDIAN,format="f",decdig)," - ",formatC(q3/REFMEDIAN,format="f",decdig),"]")),hjust = 0)
+      p<-p+geom_segment(aes(y=Y,x=q1/REFMEDIAN,yend=Y,xend=q3/REFMEDIAN*percentscalexmax),color="NA")
+    } else {
+      p<-p+geom_text(aes(y=Y,x=XMAX*xtextoffsetpercent,label=paste0(formatC(q2,format="f",decdig)," [",formatC(q1,format="f",decdig)," - ",formatC(q3,format="f",decdig),"]")),hjust = 0)
+      p<-p+geom_segment(aes(y=Y,x=q1,yend=Y,xend=q3*percentscalexmax),color="NA")
+    }
+  }
+    
+  if (freescalex_wrap) {
+    p<-p+facet_wrap(~PARAMETER,scales="free_x")
+  } else {
+    p<-p+facet_wrap(~PARAMETER)
+  }
+    
+  return(p)  
+}
