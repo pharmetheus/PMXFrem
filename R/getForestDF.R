f3c65e703c33516ef98fcb43d69643360d4c0358
Joakim Nyberg <joakim.nyberg@pharmetheus.com>
2018-11-09 17:53:38 +0100

    Added support for Parallel forest plotting

--- R/getForestDF.R
+++ R/getForestDF.R
@@ -2,7 +2,7 @@
 #'
 #' @description Get a data frame with Forest border for each univariate or multivariate covariate (and value(s)) in the input data frame
 #' @param dfCovs A data frame with covariates to include, if a covariate value is set -99 or NA, they are assumed missing and hence not include in the FFEM transformations
-#' @param cdfCovsNames A string vector 
+#' @param cdfCovsNames A string vector with names of the rows in dfCovs, if not used, names will be automaticaly assigned based on the covariate values and column names
 #' @param functionList A list of functions with input (thetas, coveffects and ...) for in which the change from the reference value will be calculated. If the function returns a vector of values, each value will be used but functionListName must contain the names with a length of all return for all functions in the functionList
 #' @param functionListName A vector of strings (names) of the parameters for each function in the functionList
 #' @param nobaseThetas the number of structural thetas in the model
@@ -14,6 +14,9 @@
 #' @param fixedSpacing A boolean (TRUE/FALSE) if fixed spacing between covariate groups should be applied, otherwise the y coordinates are calculated relative to the number of groups and numbers of covriates within a group. If fixed spacing are used, groupdist and withingroupdist will be used as well.
 #' @param groupeddist A number defining the y distance between groups of covariates
 #' @param withingroupeddist A number defining the y distance within groups of covariates
+#' @param ncores the number of cores to use for the calculations, default = 1 which means no parallellization
+#' @param cstrpackages a character vector with package names needed to run the calculations in parallel, default = NULL
+#' @param cstrExports a character vector with variables needed to run the calculations in parallel, default = NULL 
 #' @param ... additional variables to be forwarded to the the functionList functions
 #' 
 #'
@@ -27,7 +30,7 @@
 #' }
 getForestDF <- function(dfCovs,cdfCovsNames=NULL,functionList=list(function(thetas,coveffects,...){return(thetas[1]*exp(coveffects[1]))}),functionListName="PAR1",noBaseThetas, noCovThetas, noSigmas, noParCov = noBaseThetas, dfParameters,
                         parNames = paste("Par", 1:noParCov, sep = ""), covNames = paste("Cov",1:noCovThetas, sep = ""),
-                        availCov = covNames, quiet = FALSE,probs=c(0.025,0.5,0.975),dfRefRow=NULL,cGrouping=NULL,fixedSpacing=TRUE,groupdist=0.2,withingroupdist=0.1,...) {
+                        availCov = covNames, quiet = FALSE,probs=c(0.025,0.5,0.975),dfRefRow=NULL,cGrouping=NULL,fixedSpacing=TRUE,groupdist=0.2,withingroupdist=0.1,ncores=1,cstrPackages=NULL,cstrExports=NULL,...) {
 
   
   resList<-list()
@@ -51,11 +54,14 @@ getForestDF <- function(dfCovs,cdfCovsNames=NULL,functionList=list(function(thet
     return(cGroups)
   }
   if (is.null(cGrouping)) cGrouping<-getGroups(dfCovs)
+ 
+  ## Start the parallell engine if ncores > 1
+  if(ncores>1) {registerDoParallel(cores=ncores)}
   
-  counter<-1
-  for (k in 1:nrow(dfParameters)) {
+  dfres<-foreach (k=1:nrow(dfParameters),.packages = cstrPackages,.export = cstrExports,.verbose = !quiet,.combine=bind_rows) %dopar% {
     dfext<-cbind(first = 0, dfParameters[k,]) #Dummy to get same format as ext file
     thetas=as.numeric(dfext[2:(noBaseThetas+1)])
+    dfrest<-data.frame()
     for (i in 1:nrow(dfCovs)) {
       currentNames<-names(dfCovs[i,])[as.numeric(dfCovs[i,])!=-99]
       if (any(!currentNames %in% covNames$covNames)) {
@@ -64,11 +70,11 @@ getForestDF <- function(dfCovs,cdfCovsNames=NULL,functionList=list(function(thet
       }
       if (!is.null(dfRefRow)) { #Get another ref value than typical value
         ffemObjRef<-calcFFEM(noBaseThetas=noBaseThetas,noCovThetas = noCovThetas,noSigmas = noSigmas,dfext=dfext,covNames = covNames$covNames,
-                          availCov = names(dfRefRow)[as.numeric(dfRefRow)!=-99],quiet = quiet)
+                             availCov = names(dfRefRow)[as.numeric(dfRefRow)!=-99],quiet = quiet)
       }
       
       ffemObj<-calcFFEM(noBaseThetas=noBaseThetas,noCovThetas = noCovThetas,noSigmas = noSigmas,dfext=dfext,covNames = covNames$covNames,
-             availCov = names(dfCovs[i,])[as.numeric(dfCovs[i,])!=-99],quiet = quiet)
+                        availCov = names(dfCovs[i,])[as.numeric(dfCovs[i,])!=-99],quiet = quiet)
       coveffects <- rep(0,length(parNames))
       coveffects_base <- rep(0,length(parNames))
       data47_jxrtp <- dfCovs[i,]
@@ -76,11 +82,12 @@ getForestDF <- function(dfCovs,cdfCovsNames=NULL,functionList=list(function(thet
         ffem_expr<-str_replace_all(ffemObj$Expr[j],pattern = "data\\$",replacement = "data47_jxrtp$")
         if (length(names(dfCovs[i,])[as.numeric(dfCovs[i,])!=-99])!=0) coveffects[j] <- as.numeric(eval(parse(text=ffem_expr)))
         if (!is.null(dfRefRow)) {
-           ffem_expr_base<-str_replace_all(ffemObjRef$Expr[j],pattern = "data\\$",replacement = "dfRefRow$")
-           coveffects_base[j]<-as.numeric(eval(parse(text=ffem_expr_base)))
+          ffem_expr_base<-str_replace_all(ffemObjRef$Expr[j],pattern = "data\\$",replacement = "dfRefRow$")
+          coveffects_base[j]<-as.numeric(eval(parse(text=ffem_expr_base)))
         }
       }
       n=1
+      
       for (j in 1:length(functionList)) {
         val<-functionList[[j]](thetas,coveffects,...)
         if (!is.null(dfRefRow)) { ## Add a new reference line based on some covariate values
@@ -91,14 +98,13 @@ getForestDF <- function(dfCovs,cdfCovsNames=NULL,functionList=list(function(thet
         listcount<-length(val) 
         
         for (l in 1:listcount) {
-          resList[[counter]]<-data.frame(ITER=k,COVS=i,NAME=functionListName[n],VALUE=val[[l]],VALUEBASE=valbase[[l]])
-          counter<-counter+1
+          dfrest<-bind_rows(dfrest,data.frame(ITER=k,COVS=i,NAME=functionListName[n],VALUE=val[[l]],VALUEBASE=valbase[[l]]))
           n<-n+1
         }
       }
     }
+    dfrest
   }
-  dfres<-as.data.frame(data.table::rbindlist(resList))
 
   getCovNameString <- function(dfrow) {
     strName<-""
