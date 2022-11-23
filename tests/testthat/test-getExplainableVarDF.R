8172c635e372deff2cb06e999d52d941db303686
Niclas Jonsson <niclas.jonsson@pharmetheus.com>
2022-11-23 21:45:30 +0100

    Added tests for getExplainableVarDF

---
+++ tests/testthat/test-getExplainableVarDF.R
@@ -0,0 +1,105 @@
+test_that("getExplainableVarDF works", {
+
+library(forcats)
+
+modDevDir <- system.file("extdata/SimNeb",package="PMXFrem")
+fremRunno <- 31
+modFile   <- file.path(modDevDir,paste0("run",fremRunno,".mod"))
+covNames  <- getCovNames(modFile = modFile)
+
+## Set up dfCovs
+dfData <- read.csv(system.file("extdata/SimNeb/DAT-2-MI-PMX-2-onlyTYPE2-new.csv", package = "PMXFrem")) %>%
+  filter(BLQ == 0) %>%
+  distinct(ID,.keep_all = T)
+
+dfCovs <- dfData  %>%
+  select(covNames$orgCovNames) %>%
+  mutate_all(function(x) return(1)) %>%
+  slice(rep(1,ncol(.)+1))
+
+for(i in 2:nrow(dfCovs)) {
+  dfCovs[i, names(dfCovs) != names(dfCovs)[i-1]] <- -99
+}
+
+expect_snapshot_value(dfCovs,style = "deparse")
+
+cstrCovariates <- c("All",names(dfCovs))
+
+## The parameter function list
+functionList2 <- list(
+  function(basethetas,covthetas, dfrow, etas, ...){ return(basethetas[2]*exp(covthetas[1] + etas[3]))},
+  function(basethetas,covthetas, dfrow, etas, ...){ return(basethetas[3]*exp(covthetas[2] + etas[4]))}
+)
+functionListName2 <- c("CL","V")
+
+dfres0 <- getExplainableVarDF(type             = 0,
+                              data             = NULL,
+                              dfCovs           = dfCovs,
+                              numNonFREMThetas = 7,
+                              numSkipOm        = 2,
+                              functionList     = functionList2,
+                              functionListName = functionListName2,
+                              cstrCovariates   = cstrCovariates,
+                              modDevDir        = modDevDir,
+                              runno            = fremRunno,
+                              ncores           = 1,
+                              quiet            = TRUE,
+                              seed             = 123
+)
+
+expect_snapshot_value(dfres0,style = "deparse")
+
+dfres1 <- getExplainableVarDF(type             = 1,
+                              data             = dfData,
+                              dfCovs           = dfCovs,
+                              numNonFREMThetas = 7,
+                              numSkipOm        = 2,
+                              functionList     = functionList2,
+                              functionListName = functionListName2,
+                              cstrCovariates   = cstrCovariates,
+                              modDevDir        = modDevDir,
+                              runno            = fremRunno,
+                              ncores           = 10,
+                              quiet            = TRUE,
+                              seed             = 123
+)
+
+expect_snapshot_value(dfres1,style = "deparse")
+
+dfres2 <- getExplainableVarDF(type             = 2,
+                              data             = dfData,
+                              dfCovs           = dfCovs,
+                              numNonFREMThetas = 7,
+                              numSkipOm        = 2,
+                              functionList     = functionList2,
+                              functionListName = functionListName2,
+                              cstrCovariates   = cstrCovariates,
+                              modDevDir        = modDevDir,
+                              runno            = fremRunno,
+                              ncores           = 10,
+                              numETASamples    = 10,
+                              quiet            = TRUE,
+                              seed             = 123
+)
+
+expect_snapshot_value(dfres2,style = "deparse")
+
+dfres3 <- getExplainableVarDF(type             = 3,
+                              data             = dfData,
+                              dfCovs           = dfCovs,
+                              numNonFREMThetas = 7,
+                              numSkipOm        = 2,
+                              functionList     = functionList2,
+                              functionListName = functionListName2,
+                              cstrCovariates   = cstrCovariates,
+                              modDevDir        = modDevDir,
+                              runno            = fremRunno,
+                              ncores           = 10,
+                              numETASamples    = 10,
+                              quiet            = TRUE,
+                              seed             = 123
+)
+
+expect_snapshot_value(dfres3,style = "deparse")
+
+})
