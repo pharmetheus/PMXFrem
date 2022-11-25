f88681e5d822905636f70bfd042d6746e651b51e
Joakim Nyberg <joakim.nyberg@pharmetheus.com>
2022-11-25 23:32:07 +0100

    Added matrix builder function

---
+++ R/buildmatrix.R
@@ -0,0 +1,59 @@
+#' Title
+#'
+#' @param matrix the matrix to be converted
+#' @param strName name of the matrix, normally OMEGA (default) or SIGMA
+#' @param assumesame assume equal block matrix should be marked as SAME
+#'
+#' @return string vector of a NONMEM representation of the matrix
+#' @export
+#'
+#' @examples
+buildmatrix<-function(matrix,strName="OMEGA",assumesame=TRUE){
+  strres<-c()
+  getnumstr<-function(blocksize,submat) {
+    strvec=rep("",blocksize)
+    str<-""
+    if (blocksize==1 && !is.matrix(submat)) return(paste0(submat))
+    for (i in 1:blocksize) {
+      for (j in 1:i) {
+        str<-paste0(str,submat[i,j]," ")
+      }
+      strvec[i]<-str
+      str<-""
+    }
+    return(strvec)
+  }
+  checksame<-function(mat,nb,po,o,i,as) {
+    if (o==0) return("")
+    tmpmat<-as.matrix(mat[(o+1):i,(o+1):i])[1:nb,1:nb]
+    anmat<-mat[(po+1):o,(po+1):o]
+    if (((!is.matrix(tmpmat) && !is.matrix(anmat)) || 
+         (nrow(tmpmat)==nrow(anmat) && ncol(tmpmat)==ncol(anmat))) 
+        && all(tmpmat==anmat) && as==T) return("SAME")
+    return("")
+  }
+  
+  offset<-0
+  prevoffset<-0
+  numinblock<-1
+  for (i in 1:nrow(matrix)) {
+    if ((i-1)>offset && matrix[1+offset,i]!=0) numinblock<-numinblock+1
+    if ((i-1)>offset && matrix[1+offset,i]==0) {
+      strSAME<-checksame(matrix,numinblock,prevoffset,offset,i,assumesame)
+      strres<-c(strres,paste0(strName," BLOCK(",numinblock,") ",strSAME))
+      if (strSAME=="") strres<-c(strres,getnumstr(numinblock,matrix[(offset+1):i,(offset+1):i]))
+      numinblock<-1
+      prevoffset<-offset
+      offset<-i-1
+    }
+  }
+  strSAME<-checksame(matrix,numinblock,prevoffset,offset,i,assumesame)
+  strres<-c(strres,paste0(strName," BLOCK(",numinblock,") ",strSAME))
+  if (strSAME=="") strres<-c(strres,getnumstr(numinblock,matrix[(offset+1):i,(offset+1):i]))
+  return(strres)
+}
+
+#library(magic)
+#M1<-matrix(c(1,.5,.5,2),ncol=2)
+#M2<-matrix(c(3,.5,.6,1,.5,4,1,4,5),ncol=3)
+#cat(paste0(buildmatrix(adiag(matrix(3),matrix(3),M2,M1,M1,M2,M2,M1,M1,M2,M1)),"\n"))
