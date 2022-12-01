ffa2650aa017fd3239cbf3449bdb2f853ca47bc1
Niclas Jonsson <niclas.jonsson@pharmetheus.com>
2022-12-01 08:44:16 +0100

    Example for updateFREM

---
+++ updateFREMExample.R
@@ -0,0 +1,45 @@
+library(data.table)
+library(tidyverse)
+library(pmxvpc)
+library(tools)
+
+modDevDir <- "/PMX/Projects/Pharmetheus/PMX-FREMcourse-RnD-1/Analysis/Model/"
+
+fremRun <- 31
+
+updateFREM(
+  strFREMModel      = file.path(modDevDir,"run31.mod"),
+  strFREMData       = file.path(modDevDir,"frem30.dir/frem_dataset.dta"),
+  strFFEMData       = file.path(modDevDir,"../ProducedData/Dataset/DAT-2-MI-PMX-2-onlyTYPE2-new.csv"),
+  cstrRemoveCov     = c("SEX"),
+  cstrCatCovsToAdd  = NULL,
+  cstrContCovsToAdd = NULL,
+  strID             = "ID",
+  strNewFREMData    = "frem_dataset_noSEX.csv",
+  bWriteData        = FALSE,
+  quiet             = F,
+  bWriteFIX         = TRUE,
+  cstrKeepCols = c("ID","TAD","AMT","EVID","RATE","DV","FREMTYPE"))
+
+
+## The object tmp is a list and contains the Omega', names of the new columns for $INP and the name of the new data file.
+tmp <- createFFEMdata(runno            = fremRun,
+                     modDevDir        = modDevDir,
+                     numNonFREMThetas = 7,
+                     numSkipOm        = 2,
+                     #availCov         = NULL,
+                     parNames         = c("CL","V","MAT"),
+                     dataFile         = "/PMX/Projects/Pharmetheus/PMX-FREMcourse-RnD-1/Analysis/ProducedData/Dataset/DAT-2-MI-PMX-2-onlyTYPE2-new.csv",
+                     newDataFile      = NULL)
+
+
+ffemMod <- createFFEMmodel(runno            = 31,
+                           modDevDir        = modDevDir,
+                           numNonFREMThetas = 7,
+                           numSkipOm        = 2,
+                           parNames         = c("CL","V","MAT"),
+                           dataFile         = "/PMX/Projects/Pharmetheus/PMX-FREMcourse-RnD-1/Analysis/ProducedData/Dataset/DAT-2-MI-PMX-2-onlyTYPE2-new.csv",
+                           newDataFile      = "testFileName.csv",
+                           quiet            = FALSE,
+                           baserunno        = 30)
+
