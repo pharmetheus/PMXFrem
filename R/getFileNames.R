031c4571c2ce61402e46c77ac5140479f951876a
Niclas Jonsson <niclas.jonsson@pharmetheus.com>
2024-03-06 08:24:40 +0100

    Fixed documentation

--- R/getFileNames.R
+++ R/getFileNames.R
@@ -1,27 +1,33 @@
 #' getFileNames
 #'
 #' @description Assembles the model, ext, phi and lst file names for a run based on either run number or model name (the part of the file name to the left of the filename extension).
-#' @param runno The run number.
-#' @param modName The model name. Default is the current directory, i.e. ".".
-#' @param modDevDir The directory where the files reside.
+#' @param runno The run number. Must be provided if `modName` is NULL. Will be used to create file model and NONMEM results file names  with
+#' the structure run[runno].[modExt] and run[runno].[lstExt]. NONMEM ext, phi and cov files will be assumed to be named run[runno] followed by the appropriate suffix.
+#' @param modName The model name. Will be used together with `modExt`, `lstExt`, ext, phi and cov to form the NONMEM file names, for
+#' example [modName].[modExt] for the model file.
+#' @param modDevDir The directory where the files reside. Default is the current directory, i.e. ".".
 #' @param modExt The model file name extension. Default is ".mod".
 #' @param lstExt The NONMEM out file name extension. Default is ".lst".
-#' @details 
+#' @details
 #' runno and modName can not both be NULL. If both are provided, modName is used.
-#' 
-#' modDevDir can not be NULL. 
+#'
+#' modDevDir can not be NULL.
 #' @return
 #' A list with components "mod", "ext", "phi" and "lst" specifying the path names of the corresponding files.
 #' @export
 #'
 #' @examples
 #' getFilenNames(modName=9)
-#' 
-getFileNames <- function(runno=NULL,modName=NULL,modDevDir=".",modExt = ".mod",lstExt = ".lst") {
-  
+#'
+getFileNames <- function(runno     = NULL,
+                         modName   = NULL,
+                         modDevDir = ".",
+                         modExt    = ".mod",
+                         lstExt    = ".lst") {
+
   if(is.null(runno) & is.null(modName)) stop("One of runno and modName needs to have a non-null value.")
   if(is.null(modDevDir)) stop("modDevDir must be a non-NULL value.")
-  
+
     if(is.null(modName)) {
       modFile    <- file.path(modDevDir,paste0("run",runno,modExt))
       extFile    <- file.path(modDevDir,paste0("run",runno,".ext"))
@@ -35,9 +41,9 @@ getFileNames <- function(runno=NULL,modName=NULL,modDevDir=".",modExt = ".mod",l
       covFile    <- file.path(modDevDir,paste0(modName,".cov"))
       lstFile    <- file.path(modDevDir,paste0(modName,lstExt))
     }
-  
-  
+
+
   retList <- list(mod=modFile,ext=extFile,phi=phiFile,lst=lstFile,cov=covFile)
-  
+
   return(retList)
 }
