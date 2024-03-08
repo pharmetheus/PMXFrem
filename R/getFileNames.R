1eed6d6be2a95b84ce3edc6c996d85362f0e9e13
Niclas Jonsson <niclas.jonsson@pharmetheus.com>
2024-03-08 09:03:26 +0100

    Upated documentation

--- R/getFileNames.R
+++ R/getFileNames.R
@@ -4,25 +4,25 @@
 #' run number or model name (the part of the file name to the left of the
 #' filename extension).
 #'
-#' runno and modName can not both be NULL. If both are provided,
-#' modName is used.
+#' runno and modName can not both be NULL. If both are provided, modName is
+#' used.
 #'
 #' modDevDir can not be NULL.
 #'
 #' @param runno The run number. Must be provided if `modName` is NULL. Will be
 #'   used to create NONMEM model and results file names  with the structure
-#'   run[runno].[modExt] and run[runno].[lstExt]. NONMEM ext, phi and cov files
-#'   will be assumed to be named run[runno] followed by the appropriate suffix.
+#'   run#.mod and run#. NONMEM ext, phi and cov files will be assumed to be
+#'   named run# followed by the appropriate suffix (where # is `runno`).
 #' @param modName The model name. Will be used together with `modExt`, `lstExt`,
 #'   ext, phi and cov to form the NONMEM file names, for example
-#'   [modName].[modExt] for the model file.
+#'   `modName`.mod for the model file.
 #' @param modDevDir The directory where the files reside. Default is the current
 #'   directory, i.e. ".".
 #' @param modExt The model file name extension. Default is ".mod".
 #' @param lstExt The NONMEM out file name extension. Default is ".lst".
 #'
 #' @return A list with components "mod", "ext", "phi" and "lst" specifying the
-#' path names of the corresponding files.
+#'   path names of the corresponding files.
 #' @export
 #'
 #' @examples
